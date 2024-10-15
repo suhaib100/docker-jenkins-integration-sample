03dea9767b524fc19c70bf7da5ed001127524a8c
suhaib100 <khansuhaib001@gmail.com>
2024-10-15 16:48:16 +0530

    first commit

---
+++ src/main/java/com/javatechie/docker_genkins_integration_sample/DockerGenkinsIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker_genkins_integration_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerGenkinsIntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerGenkinsIntegrationSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/docker_genkins_integration_sample/DockerGenkinsIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker_genkins_integration_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerGenkinsIntegrationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
